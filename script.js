51150a8f64b10a454f83dd43646b0ad992e279c5
Rogelio Suleta <andresrogesu@gmail.com>
2023-08-10 01:19:24 -0300

    cambios en rama secundaria, pruebas

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
-
-import fs from 'fs';
-
+import { promises as fs } from 'fs';
 class ProductsManager {
     constructor() {
         this.products = [];
-        this.usedIds = new Set();
         this.loadProducts();
     }
 
 
-
-    addProduct(product) {
-        const prod = this.products.find(prod => prod.code === product.code)
-        if (prod) {
-            console.log('producto ya encontrado');
-        } else if (!this.usedIds.has(product.id)) {
-            // Verificamos si el ID aún no ha sido utilizado
-            this.products.push(product);
-            this.usedIds.add(product.id); // Agregamos el ID al conjunto de IDs utilizados
-            this.saveProducts();
-        } else {
-            console.error('Error al cargar el producto, intente nuevamente')
+    async addProduct(product) {
+        try {
+            const prodJson = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+            const prod = prodJson.find((prods) => prods.code === product.code);
+            if (prod) {
+                console.log('producto existente');
+            } else {
+                this.products.push(product);
+                this.saveProducts();
+            }
+        } catch (error) {
+            console.error(error);
         }
 
 
     }
 
-    
 
-    loadProducts() {
+
+
+
+    async loadProducts() {
         try {
-            const data = fs.readFileSync('./productos.json', 'utf8');
-            this.products = JSON.parse(data);
-            this.products.forEach(producto => this.usedIds.add(producto.id));
+            const data = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+            this.products = data;
         } catch (error) {
             this.products = [];
             this.usedIds = new Set();
+            console.log('error al cargar productos', error);
         }
     }
 
+
     saveProducts() {
         const jsonData = JSON.stringify(this.products, null, 2);
-        fs.writeFileSync('./productos.json', jsonData, 'utf8');
+        fs.writeFile('./productos.json', jsonData, 'utf8');
     }
 
-    getProducts() {
-        return this.products;
+    async getProducts() {
+        const product = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+        return product;
     }
 
 
-    getProductById(product) {
-
-        const productId = this.products.find((prod) => prod.id === product.id);
+    async getProductById(id) {
+        const prod = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+        const productId = prod.find((prod) => prod.id === id);
         if (productId) {
             console.log('producto encontrado')
-            return product;
+            console.log(productId);
         } else {
             console.log('Producto no encontrado');
             return null;
@@ -62,9 +63,25 @@ class ProductsManager {
 
 
     }
+
+    async deleteProduct(product) {
+        try {
+            const products = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+            const updatedProducts = products.filter((prod) => prod.code !== product.code);
+            const updatedProductsJson = JSON.stringify(updatedProducts)
+            await fs.writeFile('./productos.json', updatedProductsJson, null, 2), (error) => {
+                if (error) {
+                    console.error('error al escribir el archivo', error.message)
+                } else {
+                    console.log('producto eliminado exitosamente');
+                }
+            };
+
+        } catch (error) {
+            console.error('error al analizar el archivo json', error);
+        }
+    }
 }
-
-
 class Products {
     constructor(title, description, price, code, stock, thumbnail) {
         this.title = title,
@@ -89,27 +106,46 @@ class Products {
 
 
     static usedIds = new Set();
-    static generadorId() {
+    static lastGeneratedId = 0;
 
-        let idGenerado;
+    static generadorId() {
         const minId = 1;
         const maxId = 10000;
-        while (!idGenerado || this.usedIds.has(idGenerado)) {
-            idGenerado = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+
+        if (this.lastGeneratedId === 0) {
+            // Load lastGeneratedId from productos.json if available
+            try {
+                const data = fs.readFileSync('./productos.json', 'utf8');
+                const products = JSON.parse(data);
+                const lastProduct = products[products.length - 1];
+                if (lastProduct) {
+                    this.lastGeneratedId = lastProduct.id;
+                }
+            } catch (error) {
+                // No need to do anything, start from minId
+            }
         }
-        this.usedIds.add(idGenerado);
+
+        let idGenerado;
+        do {
+            idGenerado = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
+        } while (idGenerado === this.lastGeneratedId);
+
+        this.lastGeneratedId = idGenerado;
         return idGenerado;
     }
 
 }
 
 const productManager = new ProductsManager();
+productManager.saveProducts();
 
 const product1 = new Products('Pan Integral', 'Pan con harina integral y mix de semillas', 500, 'ALV100', 10, []);
-const product2 = new Products('Pan Blanco', 'Pan blanco con mix de semillas', 600, 'ALV102', 10, []);
-const product3 = new Products('Pan de Campo', 'Pan de campo con hierbas', 700, 'ALV103', 10, []);
-const product4 = new Products('Pan de masa madre', 'Pan de masa madre tipo hogaza', 800, 'ALV104', 10, []);
-const product5 = new Products('Pan de centeno', 'Pan de centeno con semillas de chia', 900, 'ALV105', 10, []);
+const product2 = new Products('Pan Blanco', 'Pan blanco con mix de semillas', 600, 'ALV101', 10, []);
+const product3 = new Products('Pan de Campo', 'Pan de campo con hierbas', 700, 'ALV102', 10, []);
+const product4 = new Products('Pan de masa madre', 'Pan de masa madre tipo hogaza', 800, 'ALV103', 10, []);
+const product5 = new Products('Pan de centeno', 'Pan de centeno con semillas de chia', 900, 'ALV104', 10, []);
+// const product52 = new Products('Pan de centeno', 'Pan de centeno con semillas de chia', 900, 'ALV105', 10, []);
 
 
 productManager.addProduct(product1);
@@ -117,10 +153,8 @@ productManager.addProduct(product2);
 productManager.addProduct(product3);
 productManager.addProduct(product4);
 productManager.addProduct(product5);
+// productManager.addProduct(product52);
 
-
-
-
-
-
+// console.log(productManager.deleteProduct(product3));
+// console.log(productManager.getProductById(product1.id));
 console.log(productManager.getProducts());
