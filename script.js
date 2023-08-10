829533ef3a32033306b6325fa7ce4fcbbbbbdf99
Rogelio Suleta <andresrogesu@gmail.com>
2023-08-10 16:41:22 -0300

    pruebas

--- script.js
+++ script.js
@@ -3,59 +3,68 @@ class ProductsManager {
     constructor() {
         this.products = [];
         this.usedIds = new Set();
-        this.loadProducts();
+        this.filePath = './productos.json';
+        this.writeProducts();
+        this.readProducts();
     }
 
-
-    async addProduct(product) {
-        try {
-            const prodJson = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
-            const prod = prodJson.find((prods) => prods.code === product.code);
-            if (prod) {
-                console.log('producto existente');
-            } else if(!this.usedIds.has(product.id)) {
-                this.products.push(product);
-                this.usedIds.add(product.id);
-                this.saveProducts();
-            }
-        } catch (error) {
-            console.error(error);
-        }
-
-
+    async writeProducts() {
+        const jsonData = JSON.stringify(this.products, null, 2);
+        await fs.writeFile(this.filePath, jsonData, 'utf8');
     }
 
-
-
-
-
-    async loadProducts() {
+    async readProducts() {
         try {
-            const data = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
+            let data = JSON.parse(await fs.readFile(this.filePath, 'utf8'));
             this.products = data;
             this.products.forEach(producto => this.usedIds.add(producto.id));
         } catch (error) {
-            this.products = [];
-            this.usedIds = new Set();
-            console.log('error al cargar productos', error);
+            if (error) {
+                this.products = [];
+                this.usedIds = new Set();
+            }
         }
     }
 
+    async addProduct(product) {
+        try {
+            //verificar si existe el producto
+            const existingProduct = this.products.find(prod => prod.code === product.code);
+            if (existingProduct) {
+                throw new Error('el producto ya existe');
+            } else {
+                this.products.push(product);
+                this.usedIds.add(product.id)
+                await this.writeProducts(this.products);
+                console.log('producto agregado');
+            }
+        } catch (error) {
+            console.error('error al agregar el producto');
+        }
 
-    async saveProducts() {
-        const jsonData = JSON.stringify(this.products, null, 2);
-        await fs.writeFile('./productos.json', jsonData, 'utf8');
     }
 
-    async getProducts() {
-        const product = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
-        return product;
-    }
+    async updatedProduct(productId, propertyName, newValue) {
+        try {
+            const productToUpdate = this.products.find((prod) => prod.id === productId);
+            if (!productToUpdate) {
+                throw new Error('producto no encontrado');
+            } else {
+                productToUpdate[propertyName] = newValue;
 
+                const index = this.products.findIndex((prod) => prod.id === productId);
+                if (index !== -1) {
+                    this.products[index] = productToUpdate;
+                }
+                await this.writeProducts();
+            }
+        } catch (error) {
+            console.error('error', error.message);
+        }
+    }
 
     async getProductById(id) {
-        const prod = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
-        const productId = prod.find((prod) => prod.id === id);
+        const productId = this.products.find((prod) => prod.id === id);
         if (productId) {
             console.log('producto encontrado')
             console.log(productId);
@@ -67,24 +76,27 @@ class ProductsManager {
 
     }
 
-    async deleteProduct(product) {
-        try {
-            const products = JSON.parse(await fs.readFile('./productos.json', 'utf8'));
-            const updatedProducts = products.filter((prod) => prod.code !== product.code);
-            const updatedProductsJson = JSON.stringify(updatedProducts)
-            await fs.writeFile('./productos.json', updatedProductsJson, null, 2), (error) => {
-                if (error) {
-                    console.error('error al escribir el archivo', error.message)
-                } else {
-                    console.log('producto eliminado exitosamente');
-                }
-            };
-
-        } catch (error) {
-            console.error('error al analizar el archivo json', error);
-        }
+    async getProducts() {
+        const product = await this.readProducts();
+        this.products = product;
+        return this.products;
     }
-}
+
+    async deleteProduct(id) {
+        try {
+            const products = this.products.find((prod) => prod.id === id);
+            if(products){
+                const prodsToDelete = this.products.filter(prod => prod.id !== id);
+                this.products = prodsToDelete;
+            }
+        } catch (error) {
+            console.error('error al eliminar el producto', error.message);
+        }
+        await this.writeProducts();
+    }
+    }
+
+
 class Products {
     constructor(title, description, price, code, stock, thumbnail) {
         this.title = title,
@@ -95,69 +107,43 @@ class Products {
             this.thumbnail = thumbnail,
             this.id = Products.generadorId();
     }
-    getProductDetails() {
-        return {
-            title: this.title,
-            description: this.description,
-            price: this.price,
-            code: this.code,
-            stock: this.stock,
-            thumbnail: this.thumbnail,
-            id: this.id
-        };
-    }
-
 
     static usedIds = new Set();
-    static lastGeneratedId = 0;
 
     static generadorId() {
-        const minId = 1;
-        const maxId = 10000;
-
-        if (this.lastGeneratedId === 0) {
-            // Load lastGeneratedId from productos.json if available
-            try {
-                const data = fs.readFileSync('./productos.json', 'utf8');
-                const products = JSON.parse(data);
-                const lastProduct = products[products.length - 1];
-                if (lastProduct) {
-                    this.lastGeneratedId = lastProduct.id;
-                }
-            } catch (error) {
-                // No need to do anything, start from minId
-            }
+        if(this.idIncrement && this.usedIds.has(this.idIncrement)){
+            this.idIncrement++;
+        } else {
+            this.idIncrement = 1;
         }
-
-        let idGenerado;
-        do {
-            idGenerado = Math.floor(Math.random() * (maxId - minId + 1)) + minId;
-        } while (idGenerado === this.lastGeneratedId);
-
-        this.lastGeneratedId = idGenerado;
-        return idGenerado;
+        this.usedIds.add(this.idIncrement);
+        return this.idIncrement;
     }
+
+
 
 }
 
 const productManager = new ProductsManager();
-productManager.saveProducts();
+productManager.writeProducts();
 
 const product1 = new Products('Pan Integral', 'Pan con harina integral y mix de semillas', 500, 'ALV100', 10, []);
 const product2 = new Products('Pan Blanco', 'Pan blanco con mix de semillas', 600, 'ALV101', 10, []);
 const product3 = new Products('Pan de Campo', 'Pan de campo con hierbas', 700, 'ALV102', 10, []);
 const product4 = new Products('Pan de masa madre', 'Pan de masa madre tipo hogaza', 800, 'ALV103', 10, []);
 const product5 = new Products('Pan de centeno', 'Pan de centeno con semillas de chia', 900, 'ALV104', 10, []);
-const product52 = new Products('Pan de centeno', 'Pan de centeno con semillas de chia', 900, 'ALV104', 10, []);
+
 
 
 productManager.addProduct(product1);
 productManager.addProduct(product2);
-productManager.addProduct(product3);
-productManager.addProduct(product4);
-productManager.addProduct(product5);
-productManager.addProduct(product52);
 
-// console.log(productManager.deleteProduct(product3));
-// console.log(productManager.getProductById(product1.id));
-console.log(productManager.getProducts());
+
+
+
+
+
+
+
+
+
